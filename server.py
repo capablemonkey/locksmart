e54744843c471a153946a58f358c19534abb9cf7
erinthered <erinbesswilliams@gmail.com>
2019-10-21 16:47:13 -0400

    add rack class and test db query

--- server.py
+++ server.py
@@ -1,14 +1,34 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
+# used by class from sqlalchemy.ext.declarative import declarative_base #used by class
+from sqlalchemy import Column, Integer, String, Float
+#from sqlalchemy.orm import sessionmaker
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 
+class Rack(db.Model):
+    index = db.Column(db.Integer, primary_key=True)
+    site_id = db.Column(db.String(255))
+    house_number = Column(String(255))
+    street_name = Column(String(255))
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+
+def get_rack_by_id(id):
+    rack = Rack.query.filter_by(site_id=id).first()
+    return rack
+
+
 @app.route('/')
 def display():
-    return "Hello World!"
+    test_rack = get_rack_by_id("46203")
+    location = test_rack.latitude+test_rack.longitude
+    return location
 
 
 @app.route('/test')
